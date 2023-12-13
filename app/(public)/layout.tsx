90c607748a21eb1f00253152576bfbf4ff8697d0
melihogul <ogul1905@gmail.com>
2023-12-13 21:22:35 +0300

    fix

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -3,7 +3,7 @@ const PublicLayout = ({
     children
 }: {children: React.ReactNode}) => {
   return (
-    <div className="h-full dark:bg-[#1f1f1f]">
+    <div className="h-full">
         {children}
     </div>
   )
